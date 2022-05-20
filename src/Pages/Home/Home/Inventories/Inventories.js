97f80662ca809e8028e02b20d133c0f4cc764cdf
U Sen Wan <senwan96@gmail.com>
2022-05-20 23:43:33 +0600

    inventory card styled

--- src/Pages/Home/Home/Inventories/Inventories.js
+++ src/Pages/Home/Home/Inventories/Inventories.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Inventory from '../Inventory/Inventory';
+import './Inventories.css'
 
 const Inventories = () => {
     const [inventories, setInventories] = useState([]);
--- src/Pages/Home/Home/Inventory/Inventory.js
+++ src/Pages/Home/Home/Inventory/Inventory.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './Inventory.css';
 
 const Inventory = ({inventory}) => {
     const {name, img, description, quantity, price, supplier} = inventory;
     return (
         <div className='inventory'>
-            <img className='w-100' src={img} alt="" />
-            <h4>{name}</h4>
-            <p>{price}</p>
-            <p>{quantity}</p>
-            <p>{supplier}</p>
-            <p><small>{description}</small></p>
+            <img className='w-100 image' src={img} alt="" />
+            <div className='details'>
+                <h4>Name: {name}</h4>
+                <h6>Price: {price}</h6>
+                <h6>Quantity: {quantity}</h6>
+                <h6>Supplier: {supplier}</h6>
+                <p>{description}</p>
+            </div>
         </div>
     );
 };
