b0585f18ec81f70b7707ceb852d751385839b8c2
U Sen Wan <senwan96@gmail.com>
2022-05-20 15:07:57 +0600

    signout implemented

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,9 +1,16 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Header.css';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <>
             <Navbar sticky='top' collapseOnSelect expand="lg" bg='primary' variant="dark">
@@ -15,9 +22,14 @@ const Header = () => {
                 </Nav>
                 <Nav>
                 <Nav.Link className='text-white menu fw-bold fs-6' as={Link} to='blogs'>Blogs</Nav.Link>
-                <Nav.Link className='btn text-white menu fw-bold fs-6' eventKey={2} as={Link} to="login">
-                        Login
-                    </Nav.Link>
+                {
+                    user ?
+                    <button onClick={handleSignOut} className='btn text-white fw-bold, fs-6 bg-primary'>Signout</button>
+                    :
+                    <Nav.Link className='btn text-white menu fw-bold fs-6' eventKey={2} as={Link} to="login">
+                    Login
+                </Nav.Link>
+                }
                 </Nav>
             </Navbar.Collapse>
         </Container>
