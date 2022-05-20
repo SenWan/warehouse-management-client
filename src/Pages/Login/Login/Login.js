0498e3b83b14d1a03848badb14f7d34e33cd08ab
U Sen Wan <senwan96@gmail.com>
2022-05-20 14:51:35 +0600

    login implemented

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Login = () => {
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  let from = location.state?.from?.pathname || '/';
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    signInWithEmailAndPassword(email, password)
+  }
+
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loadin,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  if(user){
+    navigate(from, {replace: true})
+  }
+
+  const navigateRegister = event => {
+    navigate('/register');
+  }
     return (
         <div className='container' style={{height:'60vh'}}>
         <h2 className='text-center mt-5'>Please Login</h2>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-2" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Enter email" required />
+            <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
           </Form.Group>
   
           <Form.Group className="mb-2" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" required />
+            <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
           </Form.Group>
           <Button variant="info text-white w-50 mx-auto d-block mb-2" type="submit">
             Login
           </Button>
         </Form>
-        <p>Are you new user? <Link to='/register'  style={{cursor:'pointer'}} className='text-primary text-decoration-none'> Please Register !</Link></p>
+        <p>Are you new user? <Link to='/register'  style={{cursor:'pointer'}} className='text-primary text-decoration-none' onClick={navigateRegister}> Please Register !</Link></p>
         <p>forget password? <span  style={{cursor:'pointer'}} className='text-primary'> Reset Password !</span></p>
       </div>
     );
